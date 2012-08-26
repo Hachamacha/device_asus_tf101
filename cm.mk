0fa9c3d47816101c4d6e7e088dadc748f56b07d5
RaymanFX <raymanfx@gmail.com>
2012-08-26 14:12:55 +0200

    add cm/cwm specific files

---
+++ cm.mk
@@ -0,0 +1,20 @@
+# Inherit device configuration for tf101.
+$(call inherit-product, device/asus/tf101/full_tf101.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cm_tf101
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := tf101
+PRODUCT_MODEL := Transformer
+PRODUCT_MANUFACTURER := asus
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=US_epad
+BUILD_FINGERPRINT="asus/US_epad/TF101:4.0.3/IML74K/US_epad-9.2.1.27-20120615:user/release-keys"
+PRIVATE_BUILD_DESC="US_epad-user 4.0.3 IML74K US_epad-9.2.1.27-20120615 release-keys"
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := tf101
